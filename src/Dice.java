8ddddd758d2929e1e2b8deb1231083aa83895a03
n3vk <ddr8255@autuni.ac.nz>
2024-04-10 00:43:36 +1200

    Update Dice.java

--- src/Dice.java
+++ src/Dice.java
@@ -1,6 +1,4 @@
 
-import java.util.Random;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
