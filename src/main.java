b276b7bac99888714e218076d4ec215d97c3087f
pillaypower <tgq4346@autuni.ac.nz>
2024-04-09 23:18:12 +1200

    dice method done.

--- src/main.java
+++ src/main.java
@@ -14,11 +14,19 @@ public class main {
         User user = new User();
         Dice dice= new Dice();
         board board = new Board();
+        
+        Scanner scanner = new Scanner(System.in);
        
+        System.out.println("Press 'r' to Roll Dice");
+        String input = scanner.nextLine();
         
+        while(!input.equalsIgnoreCase("r")){
+            System.out.println("Error! Input not valid");
+            input = scanner.nextLine();
+        }
         
+        int rollResult = dice.roll();
+        System.out.println(name + " rolled: " + rollResult);
         
     }
-    
-    
 }
