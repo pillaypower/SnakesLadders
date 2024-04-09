d42cd35a2a14f0a11be8f1cff8557ac8089117a3
n3vk <ddr8255@autuni.ac.nz>
2024-04-09 14:40:45 +1200

    Update SnakesAndLadders.java

--- src/snakes/and/ladders/SnakesAndLadders.java
+++ src/snakes/and/ladders/SnakesAndLadders.java
@@ -14,7 +14,7 @@ public class SnakesAndLadders {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //test
     }
     
 }
