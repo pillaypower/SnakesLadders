4953817fd994f284d7854e424edaa76160fee50c
pillaypower <tgq4346@autuni.ac.nz>
2024-04-09 14:34:51 +1200

    Create Dice.java
    dice update

---
+++ src/Dice.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ishaa
+ */
+public class Dice {
+    
+    private Random random;
+    
+    public Dice(){
+        random = new Random();
+    }
+    
+    public int roll(){
+        return random.nextInt(6) + 1;
+    }
+}
+
