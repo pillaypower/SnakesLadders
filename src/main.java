7568391b54610697a6c12471c633654d2bef92ef
n3vk <ddr8255@autuni.ac.nz>
2024-04-09 23:43:27 +1200

    added the introduction part for choosing options

--- src/main.java
+++ src/main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -14,10 +17,45 @@ public class main {
         User user = new User();
         Dice dice= new Dice();
         board board = new Board();
+        boolean exit = false;
+        
+        
+        while(!exit) {
+            System.out.println("Snakers and Ladders! (CLI Version)");
+            System.out.println("\n");
+            System.out.println("Please Choose an option");
+            System.out.println("1. Start Game");
+            System.out.println("2. View Leaderboard");
+            System.out.println("3. Exit");
+            
+            int option = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch(option) {
+                case 1:
+                //where we put the play game method
+                break;
+            
+                case 2: 
+                //where we would display leaderboard method
+                break;
+                
+                case 3:
+                exit = true;
+                System.out.println("Thank you for playing!");
+                break;
+                
+                default:
+                 System.out.println("Invalid option, Please try again. ");
+   
+            }
+        }
+        scanner.close();
+        
         
         Scanner scanner = new Scanner(System.in);
        
-        System.out.println( name + "press 'r' to Roll Dice");
+        System.out.println( name + "press 'R' to Roll Dice");
         String input = scanner.nextLine();
         
         while(!input.equalsIgnoreCase("r")){
