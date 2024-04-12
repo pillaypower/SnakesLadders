652df20b82a571f3f4369b6f241fa45cf5a8fab1
pillaypower <tgq4346@autuni.ac.nz>
2024-04-12 17:54:20 +1200

    leaderboard fixed and improved... kinda

--- src/leaderboard.java
+++ src/leaderboard.java
@@ -44,10 +44,10 @@ public class leaderboard {
       if(!scores.isEmpty()) {
            System.out.println("Leaderboard:");
            for(ScoreEntry entry : scores) {
-                System.out.println("Player:" + entry.getUserName());
-                System.out.println("Number of Moves:" + entry.getNumMoves());
-                System.out.println("Number of Snakes Bitten:" + entry.getNumSnakesBitten());  
-                System.out.println("Number of Ladders Climbed:" + entry.getNumLaddersClimbed());
+                System.out.println("Player: " + entry.getUserName());
+                System.out.println("Number of Moves: " + entry.getNumMoves());
+                System.out.println("Number of Snakes Bitten: " + entry.getNumSnakesBitten());  
+                System.out.println("Number of Ladders Climbed: " + entry.getNumLaddersClimbed());
                 System.out.println("\n");
            }
            
@@ -59,42 +59,44 @@ public class leaderboard {
        public void saveFile() {
        try(BufferedWriter writer = new BufferedWriter(new FileWriter("./resources/leaderboard.txt"))) {
           for(ScoreEntry save : scores) {
-              int totalMoves = save.getNumMoves();
-               int totalSnakesBitten = save.getNumSnakesBitten();
-               int totalLaddersClimbed = save.getNumLaddersClimbed();
-                
-               for(ScoreEntry entry : scores) {
-                   if(entry.getUserName().equals(save.getUserName())) {
-                        totalMoves += entry.getNumMoves();
-                        totalSnakesBitten += entry.getNumSnakesBitten();
-                        totalLaddersClimbed += entry.getNumLaddersClimbed();
-                   }
-               }
-                
-              writer.write("\n" + save.getUserName() + "\nNumber of Moves:" + totalMoves + "\nNumber of Snakes Bitten: " + totalSnakesBitten + "\nNumber of Ladders Climbed: " +  totalLaddersClimbed + "\n\n");
+              writer.write(" \n Username: " + save.getUserName() + "\n "
+              + "Number of Moves Made: " + save.getNumMoves() + "\n"
+               + "Number of Snakes Bitten by: " + save.getNumSnakesBitten() + "\n"
+               + "Number of Ladders Climbed: " + save.getNumLaddersClimbed() + "\n");
           }
-      } catch (IOException ex) {
-          Logger.getLogger(leaderboard.class.getName()).log(Level.SEVERE, null, ex);
-      } 
-        }
+       }
+       catch (IOException ex){
+           Logger.getLogger(leaderboard.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       }
+             
+  
         
    public void loadFile() {
        scores.clear();
        
        try(BufferedReader reader = new BufferedReader(new FileReader("./resources/leaderboard.txt"))) {
-           String blank;
-            while((blank = reader.readLine()) != null) {
-               String[] units = blank.split(",");
-               if(units.length == 5) {
+           
+           String line;
+            while((line = reader.readLine()) != null) {
+               String[] units = line.split(",");
+               if(units.length == 4) {
+               String userName = units[0];
+               int numMoves = Integer.parseInt(units[1]);
+               int numSnakesBitten = Integer.parseInt(units[2]);
+               int numLaddersClimbed = Integer.parseInt(units[3]);
+               scores.add(new ScoreEntry(userName,numMoves, numSnakesBitten, numLaddersClimbed));
                }
                
             }
+       }
        
-       } catch (FileNotFoundException ex){
+       catch (FileNotFoundException ex){
            Logger.getLogger(leaderboard.class.getName()).log(Level.SEVERE, null, ex);
-       } catch (FileNotFoundException ex){
+       }
+        catch (IOException ex){
            Logger.getLogger(leaderboard.class.getName()).log(Level.SEVERE, null, ex);
        }
        }
-   }
+}
 
